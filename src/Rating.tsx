7c918b0a27f4220351c141dcb8612d74f6742e52
Arenelin <belov.1098@mail.ru>
2023-12-03 15:51:08 +0700

    did the first task and placed the components encapsulated

---
+++ src/Rating.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Star} from './Star';
+
+export function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+
---
+++ src/Star.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Star() {
+    return (
+        <div>
+            Star
+        </div>
+    );
+}
+
