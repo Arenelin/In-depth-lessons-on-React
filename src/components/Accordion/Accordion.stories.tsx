c3a9112acdb333289a36d4fc59481e53e85c12f1
Arenelin <belov.1098@mail.ru>
2024-01-05 12:08:29 +0700

    introducing  Storybook and writing stories

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,45 @@
+import type {StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+
+import {Accordion} from './Accordion';
+import React, {useState} from 'react';
+
+
+export default {
+    component: Accordion,
+};
+type Story = StoryObj<typeof Accordion>;
+
+const onChangeCallback = () => {
+    console.log('hey hey')
+}
+export const FirstStory: Story = {
+    args: {
+        titleValue: 'Users',
+        collapsed: true,
+        onClick: () => {
+        }
+    }
+}
+export const SecondStory: Story = {
+    args: {
+        titleValue: 'Menu',
+        collapsed: false,
+        onClick: onChangeCallback
+    }
+}
+
+const onChangeHandler = action('on change');
+
+export const CollapsedAccordion = () => {
+    return <Accordion titleValue={'Collapsed Accordion'} collapsed={true} onClick={onChangeHandler}/>
+}
+export const OpenedAccordion = () => {
+    return <Accordion titleValue={'Opened Accordion'} collapsed={false} onClick={() => {
+    }}/>
+}
+export const AccordionDemo = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return <Accordion titleValue={'Demo2 Accordion'} collapsed={collapsed} onClick={setCollapsed}/>
+}
